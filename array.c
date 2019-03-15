458b7592ec4aede9609f1e161d4dc84aba87880d
a304yuan <a304yuanyuan@gmail.com>
2019-03-15 17:39:46 +0800

    use any to store value

--- array.c
+++ array.c
@@ -5,8 +5,8 @@ struct _elem_position {
     size_t idx;
 };
 
-static bucket * array_bucket_new(size_t elem_size, size_t capacity) {
-    bucket * bck = malloc(sizeof(bucket) + capacity * elem_size);
+static bucket * array_bucket_new(size_t capacity) {
+    bucket * bck = malloc(sizeof(bucket) + capacity * sizeof(any));
     bck->size = 0;
     bck->next = NULL;
     return bck;
@@ -27,16 +27,14 @@ static struct _elem_position array_find(const array * arr, size_t idx) {
         prev = cur;
         cur = cur->next;
     }
-    struct _elem_position pos = {.bck = cur, .idx = idx};
-    return pos;
+    return (struct _elem_position){.bck = cur, .idx = idx};
 }
 
-array * array_new(size_t elem_size, size_t bucket_capacity) {
+array * array_new(size_t bucket_capacity) {
     array * arr = malloc(sizeof(array));
     arr->bucket_capacity = bucket_capacity;
-    arr->elem_size = elem_size;
     arr->size = 0;
-    arr->bucket_list = array_bucket_new(elem_size, bucket_capacity);
+    arr->bucket_list = array_bucket_new(bucket_capacity);
     return arr;
 }
 
@@ -50,76 +48,72 @@ void array_free(array * arr) {
     free(arr);
 }
 
-void array_append(array * arr, const void * src) {
+void array_append(array * arr, any val) {
     bucket * bck = arr->bucket_list;
     while (bck->next) {
         bck = bck->next;
     }
     if (bck->size == arr->bucket_capacity) {
-        bucket * new_bck = array_bucket_new(arr->elem_size, arr->bucket_capacity);
+        bucket * new_bck = array_bucket_new(arr->bucket_capacity);
         bck->next = new_bck;
         bck = new_bck;
     }
-    memcpy(bck->data + bck->size * arr->elem_size, src, arr->elem_size);
+    bck->data[bck->size] = val;
     bck->size += 1;
     arr->size += 1;
 }
 
-void array_insert(array * arr, size_t idx, const void * src) {
+void array_insert(array * arr, size_t idx, any val) {
     if (idx >= arr->size) return;
 
     struct _elem_position pos = array_find(arr, idx);
     bucket * bck = pos.bck;
     if (bck->size == arr->bucket_capacity) {
-        bucket * new_bck = array_bucket_new(arr->elem_size, arr->bucket_capacity);
-        memcpy(new_bck->data, bck->data + pos.idx * arr->elem_size, (bck->size - pos.idx) * arr->elem_size);
+        bucket * new_bck = array_bucket_new(arr->bucket_capacity);
+        memcpy(new_bck->data, bck->data + pos.idx, (bck->size - pos.idx) * sizeof(any));
         new_bck->next = bck->next;
         new_bck->size = bck->size - pos.idx;
         bck->next = new_bck;
         bck->size = pos.idx;
     }
-    memcpy(bck->data + pos.idx * arr->elem_size, src, arr->elem_size);
+    bck->data[pos.idx] = val;
     bck->size += 1;
     arr->size += 1;
 }
 
-void array_get(const array * arr, size_t idx, void * dest) {
-    if (idx >= arr->size) return;
+any array_get(const array * arr, size_t idx) {
+    if (idx >= arr->size) return (any){0};
 
     struct _elem_position pos = array_find(arr, idx);
-    memcpy(dest, pos.bck->data + pos.idx * arr->elem_size, arr->elem_size);
+    return pos.bck->data[pos.idx];
 }
 
-void * array_get_ref(const array * arr, size_t idx) {
+any * array_get_ref(const array * arr, size_t idx) {
     if (idx >= arr->size) return NULL;
 
     struct _elem_position pos = array_find(arr, idx);
-    return pos.bck->data + pos.idx * arr->elem_size;
+    return pos.bck->data + pos.idx;
 }
 
-void array_set(array * arr, size_t idx, const void * src) {
+void array_set(array * arr, size_t idx, any val) {
     if (idx >= arr->size) return;
 
     struct _elem_position pos = array_find(arr, idx);
-    memcpy(pos.bck->data + pos.idx * arr->elem_size, src, arr->elem_size);
+    pos.bck->data[pos.idx] = val;
 }
 
-void array_delete(array * arr, size_t idx, void * dest) {
+void array_delete(array * arr, size_t idx) {
     if (idx >= arr->size) return;
 
     struct _elem_position pos = array_find(arr, idx);
     bucket * bck = pos.bck;
-    if (dest) {
-        memcpy(dest, bck->data + pos.idx * arr->elem_size, arr->elem_size);
-    }
-    memmove(bck->data + pos.idx * arr->elem_size, bck->data + (pos.idx + 1) * arr->elem_size, (bck->size - pos.idx - 1) * arr->elem_size);
+    memmove(bck->data + pos.idx, bck->data + pos.idx + 1, (bck->size - pos.idx - 1) * sizeof(any));
     bck->size -= 1;
     arr->size -= 1;
 }
 
 void array_iter_init(const array * arr, array_iter * iter) {
     iter->bck = arr->bucket_list;
-    iter->elem_size = arr->elem_size;
     iter->next_ref = arr->bucket_list->data;
     iter->idx = 0;
     iter->idx_in_bck = 0;
@@ -129,21 +123,8 @@ bool array_iter_has(const array_iter * iter) {
     return iter->bck && iter->idx_in_bck < iter->bck->size;
 }
 
-size_t array_iter_next(array_iter * iter, void * dest) {
-    memcpy(dest, iter->next_ref, iter->elem_size);
-    if (++iter->idx_in_bck == iter->bck->size) {
-        iter->bck = iter->bck->next;
-        iter->next_ref = iter->bck->data;
-        iter->idx_in_bck = 0;
-    }
-    else {
-        iter->next_ref += iter->elem_size;
-    }
-    return iter->idx++;
-}
-
-size_t array_iter_next_ref(array_iter * iter, void ** dest) {
-    *dest = iter->next_ref;
+any * array_iter_next_ref(array_iter * iter) {
+    any * e = iter->next_ref;
     if (++iter->idx_in_bck == iter->bck->size) {
         iter->bck = iter->bck->next;
         if (iter->bck) {
@@ -152,7 +133,11 @@ size_t array_iter_next_ref(array_iter * iter, void ** dest) {
         iter->idx_in_bck = 0;
     }
     else {
-        iter->next_ref += iter->elem_size;
+        iter->next_ref += 1;
     }
-    return iter->idx++;
+    return e;
+}
+
+any array_iter_next(array_iter * iter) {
+    return *array_iter_next_ref(iter);
 }
--- array.h
+++ array.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include "simple-any/any.h"
 
 typedef struct bucket bucket;
 typedef struct array array;
@@ -12,36 +13,34 @@ typedef struct array_iter array_iter;
 struct bucket {
     size_t size;
     bucket * next;
-    char data[];
+    any data[];
 };
 
 struct array {
     size_t bucket_capacity;
     size_t size;
-    size_t elem_size;
     bucket * bucket_list;
 };
 
 struct array_iter {
     bucket * bck;
-    void * next_ref;
-    size_t elem_size;
+    any * next_ref;
     size_t idx;
     size_t idx_in_bck;
 };
 
-extern array * array_new(size_t elem_size, size_t bucket_capacity);
+extern array * array_new(size_t bucket_capacity);
 extern void array_free(array * arr);
-extern void array_append(array * arr, const void * src);
-extern void array_insert(array * arr, size_t idx, const void * src);
-extern void array_get(const array * arr, size_t idx, void * dest);
-extern void * array_get_ref(const array * arr, size_t idx);
-extern void array_set(array * arr, size_t idx, const void * src);
-extern void array_delete(array * arr, size_t idx, void * dest);
+extern void array_append(array * arr, any val);
+extern void array_insert(array * arr, size_t idx, any val);
+extern any array_get(const array * arr, size_t idx);
+extern any * array_get_ref(const array * arr, size_t idx);
+extern void array_set(array * arr, size_t idx, any val);
+extern void array_delete(array * arr, size_t idx);
 
 extern void array_iter_init(const array * arr, array_iter * iter);
 extern bool array_iter_has(const array_iter * iter);
-extern size_t array_iter_next(array_iter * iter, void * dest);
-extern size_t array_iter_next_ref(array_iter * iter, void ** dest);
+extern any array_iter_next(array_iter * iter);
+extern any * array_iter_next_ref(array_iter * iter);
 
 #endif /* end of include guard: SIMPLE-ARRAY */
